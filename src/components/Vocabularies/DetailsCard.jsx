0bf1457f54c009d1bb29ef64b4676862be5d38ee
Sojib Ahmed <mdaminurislamsojib2006@gmail.com>
2025-09-05 00:57:59 +0600

    update Modal

--- src/components/Vocabularies/DetailsCard.jsx
+++ src/components/Vocabularies/DetailsCard.jsx
@@ -26,33 +26,42 @@ const DetailsCard = ({ id }) => {
     <>
       <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
         <div className="modal-box">
-          <div>
+          <div className="border-2 border-[#EDF7FF] p-5">
             <h3 className="font-semibold text-3xl">
               {data?.word} (<FontAwesomeIcon icon={faMicrophoneLines} />{' '}
               {data?.pronunciation} ){' '}
             </h3>
             <div>
-              <p className="py-4">Meaning</p>
+              <p className=" text-2xl font-extrabold mt-8 mb-2">Meaning</p>
               <p className=" bangla text-2xl font-medium">{data?.meaning}</p>
             </div>
             <div>
-              <h1>Example</h1>
-              <p>{data?.sentence}</p>
+              <h1 className=" text-2xl font-semibold mt-8 mb-2">Example</h1>
+              <p className=" text-2xl">{data?.sentence}</p>
+            </div>
+            <div>
+              <h1 className="bangla text-2xl font-medium mt-8 mb-2">
+                সমার্থক শব্দ গুলো
+              </h1>
+              <div className=" flex gap-1.5">
+                {data?.synonyms.map((s, index) => (
+                  <div
+                    className=" bg-[#D7E4EF] py-1.5 px-5 rounded text-[20px]"
+                    key={index}
+                  >
+                    {s}
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
 
-          <div>
-            <h1 className="bangla">সমার্থক শব্দ গুলো</h1>
-            <div className=" flex gap-1.5">
-              {data?.synonyms.map((s, index) => (
-                <div key={index}>{s}</div>
-              ))}
-            </div>
-          </div>
           <div className="modal-action">
             <form method="dialog">
               {/* if there is a button in form, it will close the modal */}
-              <button className="btn">Complete Learning</button>
+              <div className="flex justify-start">
+                <button className="btn btn-primary">Complete Learning</button>
+              </div>
             </form>
           </div>
         </div>

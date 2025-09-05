772abd08226dda2454584fc680f76188c513be49
Sojib Ahmed <mdaminurislamsojib2006@gmail.com>
2025-09-05 09:59:02 +0600

    update selecting lesson

--- src/components/Vocabularies/SelectingLesson.jsx
+++ src/components/Vocabularies/SelectingLesson.jsx
@@ -1,7 +1,7 @@
 const SelectingLesson = () => {
   return (
     <>
-      <div className="mx-auto flex gap-3 items-center flex-col bg-gray-400 p-12 mt-5 w-[80%] rounded-2xl">
+      <div className="mx-auto flex gap-3 items-center flex-col bg-gray-100 p-12 mt-10 w-[80%] rounded-2xl">
         <h1 className=" bangla text-gray-500 text-sm">
           আপনি এখনো কোন Lesson Select করেন নাই{' '}
         </h1>
